a21d9c3d60b427d86206d2de2f69f3bd888e364a
Mariana Dekhtiarenko <mariana.dekhtiarenko@ukma.edu.ua>
2025-03-29 20:47:49 +0200

    added code to input and output files

---
+++ project_template/app/io/input.py
@@ -0,0 +1,19 @@
+def input_text_from_console():
+    """
+    Function to input text from the console.
+    """
+    pass
+
+
+def read_from_file_builtin():
+    """
+    Function to read from a file using Python's built-in capabilities.
+    """
+    pass
+
+
+def read_from_file_pandas():
+    """
+    Function to read from a file using the pandas library.
+    """
+    pass
---
+++ project_template/app/io/output.py
@@ -0,0 +1,12 @@
+def output_text_to_console():
+    """
+    Function to output text to the console.
+    """
+    pass
+
+
+def write_to_file_builtin():
+    """
+    Function to write to a file using Python's built-in capabilities.
+    """
+    pass
